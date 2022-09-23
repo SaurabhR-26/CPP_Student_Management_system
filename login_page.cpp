0870864d7c454fd60fdcad4a6330558ac1acfc5c
Saurabh <saurabhrajopadhye26@gmail.com>
2022-09-23 15:23:34 +0530

    integrated the timetable and created teacher dashboard

--- login_page.cpp
+++ login_page.cpp
@@ -10,6 +10,7 @@
 #include "student_registration.h"
 
 using namespace std;
+#define l l lon long;
 
 int user_counter = 0;
 const string admin_username = "S@ur@bh";
@@ -21,6 +22,252 @@ int forget();
 int welcome_page();
 int failure_page();
 void admin_login();
+void printBorder();
+void show();
+void modify();
+int choice();
+
+class timetable
+{
+public:
+    struct day
+    {
+
+        string sub1;
+    };
+    int i, j;
+    struct day time[35];
+    void fun()
+    {
+
+        system("cls");
+        gotoxy(6, 1);
+        cout << "    _____________________________________________________________________________________________" << endl;
+        gotoxy(6, 2);
+        cout << "    |    MONDAY     |    TUESDAY      |     WEDNESDAY    |      THURSDAY       |     FRIDAY     |" << endl;
+        gotoxy(6, 3);
+        cout << "    |_______________|_________________|__________________|_____________________|________________|" << endl;
+        for (i = 6; i < 25; i += 3)
+        {
+            gotoxy(11, i);
+            cout << "_______________" << endl;
+            ;
+        }
+        for (i = 6; i < 25; i += 3)
+        {
+            gotoxy(27, i);
+            cout << "_________________" << endl;
+            ;
+        }
+        for (i = 6; i < 25; i += 3)
+        {
+            gotoxy(45, i);
+            cout << "__________________" << endl;
+            ;
+        }
+        for (i = 6; i < 25; i += 3)
+        {
+            gotoxy(64, i);
+            cout << "_____________________" << endl;
+            ;
+        }
+        for (i = 6; i < 25; i += 3)
+        {
+            gotoxy(86, i);
+            cout << "________________" << endl;
+            ;
+        }
+        for (i = 4; i < 25; i++)
+        {
+            gotoxy(10, i);
+            cout << "|";
+        }
+        for (i = 4; i < 25; i++)
+        {
+            gotoxy(26, i);
+            cout << "|";
+        }
+        for (i = 4; i < 25; i++)
+        {
+            gotoxy(44, i);
+            cout << "|";
+        }
+        for (i = 4; i < 25; i++)
+        {
+            gotoxy(63, i);
+            cout << "|";
+        }
+        for (i = 4; i < 25; i++)
+        {
+            gotoxy(85, i);
+            cout << "|";
+        }
+        for (i = 4; i < 25; i++)
+        {
+            gotoxy(102, i);
+            cout << "|";
+        }
+
+        for (i = 5, j = 0; i < 25, j < 7; j++, i += 3)
+        {
+            gotoxy(13, i);
+
+            cin >> time[j].sub1;
+        }
+        for (i = 5, j = 7; i < 25, j < 14; j++, i += 3)
+        {
+            gotoxy(29, i);
+
+            cin >> time[j].sub1;
+        }
+        for (i = 5, j = 14; i < 25, j < 21; j++, i += 3)
+        {
+            gotoxy(47, i);
+
+            cin >> time[j].sub1;
+        }
+        for (i = 5, j = 21; i < 25, j < 28; j++, i += 3)
+        {
+            gotoxy(66, i);
+
+            cin >> time[j].sub1;
+        }
+        for (i = 5, j = 28; i < 25, j < 35; j++, i += 3)
+        {
+            gotoxy(88, i);
+
+            cin >> time[j].sub1;
+        }
+    }
+    void display()
+    {
+        system("cls");
+        gotoxy(6, 1);
+        cout << "    _____________________________________________________________________________________________" << endl;
+        gotoxy(6, 2);
+        cout << "    |    MONDAY     |    TUESDAY      |     WEDNESDAY    |      THURSDAY       |     FRIDAY     |" << endl;
+        gotoxy(6, 3);
+        cout << "    |_______________|_________________|__________________|_____________________|________________|" << endl;
+        for (i = 6; i < 25; i += 3)
+        {
+            gotoxy(11, i);
+            cout << "_______________" << endl;
+            ;
+        }
+        for (i = 6; i < 25; i += 3)
+        {
+            gotoxy(27, i);
+            cout << "_________________" << endl;
+            ;
+        }
+        for (i = 6; i < 25; i += 3)
+        {
+            gotoxy(45, i);
+            cout << "__________________" << endl;
+            ;
+        }
+        for (i = 6; i < 25; i += 3)
+        {
+            gotoxy(64, i);
+            cout << "_____________________" << endl;
+            ;
+        }
+        for (i = 6; i < 25; i += 3)
+        {
+            gotoxy(86, i);
+            cout << "________________" << endl;
+            ;
+        }
+        for (i = 4; i < 25; i++)
+        {
+            gotoxy(10, i);
+            cout << "|";
+        }
+        for (i = 4; i < 25; i++)
+        {
+            gotoxy(26, i);
+            cout << "|";
+        }
+        for (i = 4; i < 25; i++)
+        {
+            gotoxy(44, i);
+            cout << "|";
+        }
+        for (i = 4; i < 25; i++)
+        {
+            gotoxy(63, i);
+            cout << "|";
+        }
+        for (i = 4; i < 25; i++)
+        {
+            gotoxy(85, i);
+            cout << "|";
+        }
+        for (i = 4; i < 25; i++)
+        {
+            gotoxy(102, i);
+            cout << "|";
+        }
+
+        for (i = 5, j = 0; i < 25, j < 7; j++, i += 3)
+        {
+            gotoxy(13, i);
+
+            cout << time[j].sub1;
+        }
+        for (i = 5, j = 7; i < 25, j < 14; j++, i += 3)
+        {
+            gotoxy(29, i);
+
+            cout << time[j].sub1;
+        }
+        for (i = 5, j = 14; i < 25, j < 21; j++, i += 3)
+        {
+            gotoxy(47, i);
+
+            cout << time[j].sub1;
+        }
+        for (i = 5, j = 21; i < 25, j < 28; j++, i += 3)
+        {
+            gotoxy(66, i);
+
+            cout << time[j].sub1;
+        }
+        for (i = 5, j = 28; i < 25, j < 35; j++, i += 3)
+        {
+            gotoxy(88, i);
+
+            cout << time[j].sub1;
+        }
+    }
+};
+
+timetable fy, sy, ty, finy;
+
+void message()
+{
+    int n;
+    string tmp;
+    system("cls");
+    printBorder();
+    gotoxy(5, 8);
+    cout << "ENTER MESSAGE TO THE STUDENTS :";
+    gotoxy(10, 10);
+    cout << "Enter Number of Lines to Write :";
+    cin >> n;
+    gotoxy(10, 12);
+    cout << "Message:";
+    while (n--)
+    {
+        ofstream msg("teacher_message.txt", ios_base::app);
+        getline(cin, tmp);
+        msg << tmp << endl;
+    }
+    gotoxy(30, 20);
+    cout << "----------:Message Written Successfully :--------";
+    sleep(2);
+    system("cls");
+}
 
 int welcome_page()
 {
@@ -75,12 +322,12 @@ reenter:
     cout << "\t\t\t\t\t|    USER LOGIN     |" << endl;
     cout << "\t\t\t\t\t|___________________|" << endl
          << endl;
-    // if (user_counter==0)
-    // {
-    //     cout<<"\t\t\t\tPLEASE REGISTER FIRST!"<<endl<<endl;
-    //     return 0;
-
-    // }
+    if (user_counter == 0)
+    {
+        cout << "\t\t\t\tPLEASE REGISTER FIRST!" << endl
+             << endl;
+        return 0;
+    }
     cout << "\t\t\t\tUSERNAME : ";
     cin >> username;
     cout << endl;
@@ -473,6 +720,296 @@ void admin_login()
     }
 }
 
+void teacher_dashboard()
+{
+    int cho;
+wrong:
+    int opt;
+    system("cls");
+    printBorder();
+    gotoxy(47, 4);
+    cout << "-:TEACHER DASHBOARD:-";
+    gotoxy(50, 8);
+    cout << "MENU";
+    gotoxy(40, 10);
+    cout << "1.Display Personal Information";
+    gotoxy(40, 11);
+    cout << "2.Display Timetable";
+    gotoxy(40, 12);
+    cout << "3.Modify Timetable";
+    gotoxy(40, 13);
+    cout << "4.Message To Students";
+    gotoxy(50, 15);
+    cout << "YOUR CHOICE :";
+    cin >> opt;
+
+    if (opt < 1 || opt > 5)
+    {
+        gotoxy(35, 17);
+        cout << "Invalid Option !!!";
+        sleep(3);
+        goto wrong;
+    }
+    else
+    {
+        switch (opt)
+        {
+        // case 1:
+        //     display_teacher_info();
+        //     break;
+        case 2:
+            cho = choice();
+            if (cho == 1)
+            {
+                fy.display();
+            }
+            else if (cho == 2)
+            {
+                sy.display();
+            }
+            else if (cho == 3)
+            {
+                ty.display();
+            }
+            else
+                finy.display();
+            break;
+        case 3:
+            cho = choice();
+            if (cho == 1)
+            {
+                fy.fun();
+            }
+            else if (cho == 2)
+            {
+                sy.fun();
+            }
+            else if (cho == 3)
+            {
+                ty.fun();
+            }
+            else
+                finy.fun();
+            break;
+        case 4:
+            message();
+            break;
+        }
+    }
+    getch();
+}
+
+void teacher_login()
+{
+    int x, y;
+    // printing the required format
+wrong:
+    system("cls");
+    {
+        int x, y;
+        x = 35;
+        y = 7;
+        while (x < 75)
+        {
+            cout << "_";
+            x++;
+            gotoxy(x, y);
+        }
+
+        x = 35;
+        y = 10;
+        while (x <= 75)
+        {
+            cout << "_";
+            x++;
+            gotoxy(x, y);
+        }
+
+        gotoxy(35, 8);
+        cout << "|";
+        gotoxy(35, 9);
+        cout << "|";
+        gotoxy(35, 10);
+        cout << "|";
+        gotoxy(75, 8);
+        cout << "|";
+        gotoxy(75, 9);
+        cout << "|";
+        gotoxy(75, 10);
+        cout << "|";
+
+        // printing the required info
+        gotoxy(48, 9);
+        cout << "TEACHER LOGIN";
+
+        gotoxy(35, 14);
+        cout << "USERNAME :";
+        gotoxy(35, 18);
+        cout << "PASSWORD :";
+
+        gotoxy(50, 14);
+        cout << "|";
+        gotoxy(75, 14);
+        cout << " |";
+        gotoxy(75, 15);
+        cout << " |";
+        gotoxy(50, 15);
+        cout << "|";
+
+        gotoxy(50, 18);
+        cout << "|";
+        gotoxy(75, 18);
+        cout << " |";
+        gotoxy(75, 19);
+        cout << " |";
+        gotoxy(50, 19);
+        cout << "|";
+
+        x = 50;
+        y = 13;
+        while (x < 75)
+        {
+            cout << "_";
+            x++;
+            gotoxy(x, y);
+        }
+
+        x = 50;
+        y = 15;
+        while (x < 75)
+        {
+            cout << "_";
+            x++;
+            gotoxy(x, y);
+        }
+
+        x = 50;
+        y = 17;
+        while (x < 75)
+        {
+            cout << "_";
+            x++;
+            gotoxy(x, y);
+        }
+
+        x = 50;
+        y = 19;
+        while (x <= 75)
+        {
+            cout << "_";
+            x++;
+            gotoxy(x, y);
+        }
+    }
+
+    string ur, pw;
+    int pass_cnt = 0;
+    int flag = 0;
+    string user;
+    char ch;
+    char pass[25];
+    gotoxy(52, 14);
+    cin >> user;
+
+    gotoxy(52, 18);
+    int i = 0;
+    while (1)
+    {
+        ch = getche();
+        if (ch == 13)
+        {
+            break;
+        }
+        if (ch == 8)
+        {
+            cout << "\b ";
+        }
+        pass[i] = ch;
+        printf("\b*");
+        i++;
+    }
+    pass[i] = '\0';
+
+    ifstream examine;
+    examine.open("teacher_login.txt");
+    while (examine.eof() == 0)
+    {
+        examine >> ur;
+        examine >> pw;
+        if (ur == user && pw == pass)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    examine.close();
+    if (flag == 1)
+        teacher_dashboard();
+    else
+        goto wrong;
+}
+
+int choice()
+{
+    int n;
+    system("cls");
+    gotoxy(20, 17);
+    cout << "______________________" << endl;
+
+    gotoxy(20, 18);
+    cout << "| enter the choice:  |" << endl;
+    gotoxy(20, 19);
+    cout << "|____________________|" << endl;
+    gotoxy(54, 17);
+    cout << " ___________" << endl;
+    gotoxy(55, 18);
+    cout << "|" << endl;
+    gotoxy(65, 18);
+    cout << "|" << endl;
+    gotoxy(55, 19);
+    cout << "|_________|" << endl;
+    gotoxy(51, 18);
+    gotoxy(20, 2);
+    cout << "______________________"
+         << "\t\t"
+         << "__________" << endl;
+    for (int i = 3; i < 15; i += 3)
+    {
+        gotoxy(20, i);
+        cout << "|                    |"
+             << "\t\t"
+             << "|        |" << endl;
+        gotoxy(20, i + 1);
+        cout << "|                    |"
+             << "\t\t"
+             << "|        |" << endl;
+        gotoxy(20, i + 2);
+        cout << "|____________________|"
+             << "\t\t"
+             << "|________|" << endl;
+    }
+    gotoxy(22, 4);
+    cout << "First Year";
+    gotoxy(22, 7);
+    cout << "second Year";
+    gotoxy(22, 10);
+    cout << "Third Year";
+    gotoxy(22, 13);
+    cout << "Fourth Year";
+    gotoxy(60, 4);
+    cout << "1";
+    gotoxy(60, 7);
+    cout << "2";
+    gotoxy(60, 10);
+    cout << "3";
+    gotoxy(60, 13);
+    cout << "4";
+    gotoxy(60, 18);
+
+    cin >> n;
+    return n;
+}
+
 int main()
 {
     int choice, val;
@@ -494,7 +1031,8 @@ int main()
             forget();
             break;
         case 4:
-
+            teacher_login();
+            break;
         case 5:
             system("cls");
             register_teacher();
